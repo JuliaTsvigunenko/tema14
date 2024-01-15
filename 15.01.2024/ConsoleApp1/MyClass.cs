99a504850af572191e942d7446b699ea75803a35
JuliaTsvigunenko <122933386+JuliaTsvigunenko@users.noreply.github.com>
2024-01-15 09:40:03 +0200

    Add files via upload

---
+++ 15.01.2024/ConsoleApp1/MyClass.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class MyClass
+    {
+
+        /// <summary>
+        /// создание поля номера
+        /// </summary>
+        public static int number;
+
+
+        
+        /// <summary>
+        /// создание конструктора по умолчанию
+        /// </summary>
+        public void Print()
+        {
+            Console.WriteLine(number);
+        }
+
+        public const int a = 125;
+
+
+        /// <summary>
+        ///  Статический конструктор выполняется только один раз независимо от количества созданных объектов 
+        ///  Статический конструктор нельзя вызвать в ручную, он вызыввается автоматически
+        ///  Статический конструктор не имеет модификатора доступа
+        ///  В статическом конструкторе никогда не бывает параметров (скобки пустые)
+        ///  В сттатическом конструкторе, как и в статическом методе нельзя использовать ключесвое слово this для ссылки на текущий объект и можно обращаться только к статическим членам класса  
+        /// </summary>
+        static MyClass()
+        {
+            Console.WriteLine("Статический конструктор");
+            number = 1;
+        }
+
+        public MyClass()
+        {
+            Console.WriteLine("RR");
+            number = 2;
+        }
+
+
+
+    }
+}
---
+++ 15.01.2024/ConsoleApp1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //1
+
+            //MyClass instance1 = new MyClass();
+            //MyClass instance2 = new MyClass();
+            //instance1.number = 10;
+            //instance2.number = 20;
+            //instance1.Print();
+            //instance2.Print();
+
+            //2вариант
+
+            //MyClass instance1 = new MyClass();
+            //MyClass instance2 = new MyClass();
+            //MyClass.number = 10;
+            //MyClass.number = 20;
+            //instance1.Print();
+            //instance2.Print();
+            //Console.WriteLine(MyClass.a);
+
+            //3
+
+            MyClass myClass = new MyClass();
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+        }
+    }
+}
